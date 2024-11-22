955bf08047195e2a4f39f88d9124c2efcd0c4c50
Nina Bucic <bucicnina@gmail.com>
2024-11-22 15:16:17 +0100

    Added methods to display all projects with their tasks and to add a new project

--- Project_manager_app/Classes/ProjectManager.cs
+++ Project_manager_app/Classes/ProjectManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_manager_app.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,5 +15,127 @@ namespace Project_manager_app.Classes
         {
             _projects = projects;
         }
+
+        public static string GetValidStringInput()
+        {
+            string input = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                Console.Write("Input cannot be empty. Please try again: ");
+                input = Console.ReadLine();
+            }
+            return input;
+        }
+
+        public static DateTime GetValidDateInput()
+        {
+            string input = Console.ReadLine();
+            DateTime date;
+            while (!DateTime.TryParseExact(input, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out date))
+            {
+                Console.Write("Invalid date format. Please enter a valid date (YYYY-MM-DD): ");
+                input = Console.ReadLine();
+            }
+            return date;
+        }
+
+        public static ProjectStatus GetValidProjectStatus()
+        {
+            string input = Console.ReadLine();
+            while (input.ToLower() != "active" && input.ToLower() != "pending" && input.ToLower() != "completed")
+            {
+                Console.Write("Invalid input. Enter (Active, Pending, Completed): ");
+                input = Console.ReadLine();
+            }
+            return (ProjectStatus)Enum.Parse(typeof(ProjectStatus), input, true);
+        }
+
+        private bool IsProjectNameTaken(string projectName)
+        {
+            return _projects.Keys.Any(p => p.Name.Equals(projectName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void PrintAllProjectsWithTasks()
+        {
+            if (_projects.Count == 0)
+            {
+                Console.WriteLine("No projects available.");
+                Console.WriteLine("Press any key to return...");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach (var projectEntry in _projects)
+            {
+                var project = projectEntry.Key;
+                var tasks = projectEntry.Value;
+
+                Console.WriteLine($"Project: {project.Name}");
+                Console.WriteLine($"Description: {project.Description}");
+                Console.WriteLine($"Start Date: {project.StartDate.ToShortDateString()}");
+                Console.WriteLine($"End Date: {project.EndDate.ToShortDateString()}");
+                Console.WriteLine($"Status: {project.Status}");
+                Console.WriteLine("Tasks:");
+
+                if (tasks.Count == 0)
+                {
+                    Console.WriteLine("  No tasks available.");
+                }
+                else
+                {
+                    foreach (var task in tasks)
+                    {
+                        Console.WriteLine($"  Task: {task.Name}");
+                        Console.WriteLine($"    Description: {task.Description}");
+                        Console.WriteLine($"    Due Date: {task.DueDate.ToShortDateString()}");
+                        Console.WriteLine($"    Status: {task.Status}");
+                        Console.WriteLine($"    Expected Duration: {task.ExpectedDurationMinutes} minutes");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Press any key to return...");
+            Console.ReadKey();
+        }
+
+        public void AddNewProject()
+        {
+            Console.Write("Enter the name of the new project: ");
+            var name = GetValidStringInput();
+
+            while (IsProjectNameTaken(name))
+            {
+                Console.Write("Project name already exists. Please enter a different name: ");
+                name = GetValidStringInput();
+            }
+
+            Console.Write("Enter the description of the new project: ");
+            var description = GetValidStringInput();
+
+            Console.Write("Enter the start date (YYYY-MM-DD): ");
+            var startDate = GetValidDateInput();
+
+            Console.Write("Enter the end date (YYYY-MM-DD): ");
+            var endDate = GetValidDateInput();
+            while (endDate < startDate)
+            {
+                Console.Write("End date must be later than start date. Please try again: ");
+                endDate = GetValidDateInput();
+            }
+
+            Console.Write("Enter the status of the project (Active, Pending or Completed):");
+            var status = GetValidProjectStatus();
+
+            var newProject = new Project(name, description, startDate, endDate, status);
+            _projects.Add(newProject, new List<Task>());
+
+            Console.Clear();
+            Console.WriteLine($"Project '{newProject.Name}' has been added successfully.");
+            Console.WriteLine("Press any key to return...");
+            Console.ReadKey();
+        }
+
+
     }
 }
--- Project_manager_app/Program.cs
+++ Project_manager_app/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Project_manager_app.Classes;
+using Project_manager_app.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task = Project_manager_app.Classes.Task;
+using TaskStatus = Project_manager_app.Enums.TaskStatus;
+
 
 namespace Project_manager_app
 {
@@ -18,6 +21,73 @@ namespace Project_manager_app
             ProjectManager projectManager = new ProjectManager(projects);
             TaskManager taskManager = new TaskManager();
 
+            var project1 = new Project("Website Redesign", "Redesign and update the company website.", DateTime.Now, DateTime.Now.AddMonths(1), ProjectStatus.Active);
+            var project2 = new Project("Mobile App Development", "Develop a mobile app for online store.", DateTime.Now.AddMonths(1), DateTime.Now.AddMonths(2), ProjectStatus.Pending);
+            var project3 = new Project("Marketing Campaign for Product Launch", "Marketing campaign for the launch of a new product.", DateTime.Now.AddMonths(2), DateTime.Now.AddMonths(3), ProjectStatus.Completed);
+
+            var task1 = new Task("Design New Homepage Layout", "Create a new homepage layout for the website.", DateTime.Now.AddDays(2), TaskStatus.Active, 240, project1);
+            var task2 = new Task("Test Mobile App Beta Version", "Test the beta version of the mobile app on various devices.", DateTime.Now.AddDays(3), TaskStatus.Active, 180, project2);
+            var task3 = new Task("Create Marketing Content for Social Media", "Create content for marketing campaign on social media platforms.", DateTime.Now.AddDays(5), TaskStatus.Completed, 120, project3);
+            var task4 = new Task("Conduct HR System User Training", "Provide training for the new HR system users.", DateTime.Now.AddDays(7), TaskStatus.Postponed, 150, project1);
+            var task5 = new Task("Optimize Website Speed", "Increase website loading speed by optimizing images and code.", DateTime.Now.AddDays(10), TaskStatus.Active, 180, project2);
+            var task6 = new Task("Implement New Payment Gateway", "Implement a new payment gateway for the mobile app.", DateTime.Now.AddDays(12), TaskStatus.Postponed, 200, project2);
+            var task7 = new Task("Create Customer Feedback Survey", "Create a survey to collect customer feedback about the product.", DateTime.Now.AddDays(15), TaskStatus.Active, 60, project1);
+
+            projects.Add(project1, new List<Task> { task1, task4, task7 });
+            projects.Add(project2, new List<Task> { task2, task5, task6 });
+            projects.Add(project3, new List<Task> { task3 });
+
+            var exit = false;
+
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("MAIN MENU: \n");
+                Console.WriteLine("1. Display all projects with tasks");
+                Console.WriteLine("2. Add new project");
+                Console.WriteLine("3. Delete project");
+                Console.WriteLine("4. Display all tasks due in the next 7 days");
+                Console.WriteLine("5. Filter projects by status");
+                Console.WriteLine("6. Manage individual project");
+                Console.WriteLine("7. Manage individual task");
+                Console.WriteLine("0. Exit");
+
+                var choice = Console.ReadLine();
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case "1":
+                        projectManager.PrintAllProjectsWithTasks();
+                        break;
+                    case "2":
+                        projectManager.AddNewProject();
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "4":
+                        
+                        break;
+                    case "5":
+                        
+                        break;
+                    case "6":
+                        
+                        break;
+                    case "7":
+                        
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting application. Goodbye!");
+                        exit=true;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown option. Press any key to return...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
     }
 }
